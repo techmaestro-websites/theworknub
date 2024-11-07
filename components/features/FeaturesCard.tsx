934a79e9659009be2ddc0ec214de6ea3ae62a341
Taiwo Oladosu <taiwooladosu1@gmail.com>
2024-11-07 14:41:21 +0100

    text

--- components/features/FeaturesCard.tsx
+++ components/features/FeaturesCard.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image'
 const FeaturesCard = () => {
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 itens-center justify-center gap-10 px-4'>
-        <div className='flex flex-col items-center justify-center gap-5 bg-[#EAFFE3] p-10 rounded-xl hover:translate-y-[-6px] transition-all'>
+        <div className='flex flex-col items-center justify-around gap-5 bg-[#EAFFE3] p-10 rounded-xl '>
             <Image
                 src={Office}
                 alt="line"
@@ -16,7 +16,7 @@ const FeaturesCard = () => {
             <h1 className='font-bold text-xl text-center uppercase tracking-widest'>Conductive workspace</h1>
             <p className='text-center font-semibold'>Hot desk, dedicated desk, private office, meeting room, rest lounge, a conducive and innovative space</p>
         </div>
-        <div className='flex flex-col items-center justify-center gap-5 bg-[#EAFFE3] p-10 rounded-xl hover:translate-y-[-6px] transition-all'>
+        <div className='flex flex-col items-center justify-around gap-5 bg-[#EAFFE3] p-10 rounded-xl'>
             <Image
                 src={Wifi}
                 alt="line"
@@ -25,7 +25,7 @@ const FeaturesCard = () => {
             <h1 className='font-bold text-xl text-center uppercase tracking-widest'>Free and fast wi-fi</h1>
             <p className='text-center font-semibold'>Free, fast and reliable internet connection</p>
         </div>
-        <div className='flex flex-col items-center justify-center gap-5 bg-[#EAFFE3] p-10 rounded-xl hover:translate-y-[-6px] transition-all'>
+        <div className='flex flex-col items-center justify-around gap-5 bg-[#EAFFE3] p-10 rounded-xl'>
             <Image
                 src={Light}
                 alt="line"
@@ -34,7 +34,7 @@ const FeaturesCard = () => {
             <h1 className='font-bold text-xl text-center uppercase tracking-widest'>uninterrupted electricity</h1>
             <p className='text-center font-semibold'>We provide stable power supply</p>
         </div>
-        <div className='flex flex-col items-center justify-center gap-5 bg-[#EAFFE3] p-10 rounded-xl hover:translate-y-[-6px] transition-all'>
+        <div className='flex flex-col items-center justify-around gap-5 bg-[#EAFFE3] p-10 rounded-xl'>
             <Image
                 src={Car}
                 alt="line"
