f437e21b12c8b13181af6d2ca7ea78b2f1fdfead
Oluwadare Oluwatobiloba <tobidechamp15@gmail.com>
2025-04-14 00:57:31 +0100

    Blog implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Features from "@/components/features/Features"
-import Footer from "@/components/footer/Footer"
-import GalleryContainer from "@/components/gallery/GalleryContainer"
-import HeroContainer from "@/components/hero/HeroContainer"
-import Pricing from "@/components/pricing/Pricing"
-import SpacesContainer from "@/components/spaces/SpacesContainer"
+import Blog from "@/components/blog/Blog";
+import Features from "@/components/features/Features";
+import Footer from "@/components/footer/Footer";
+import GalleryContainer from "@/components/gallery/GalleryContainer";
+import HeroContainer from "@/components/hero/HeroContainer";
+import Pricing from "@/components/pricing/Pricing";
+import SpacesContainer from "@/components/spaces/SpacesContainer";
 
 const HomePage = () => {
   return (
@@ -12,9 +13,10 @@ const HomePage = () => {
       <Features />
       <SpacesContainer />
       <Pricing />
+      <Blog />
       <GalleryContainer />
       <Footer />
     </>
-  )
-}
-export default HomePage
+  );
+};
+export default HomePage;
